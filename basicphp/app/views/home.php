989c704b08f7e209e5b82ab94537f0dea80f03c3
myanmarlinks <myanmarlinks.net@gmail.com>
2014-02-22 11:00:11 +0630

    Add View Layer

--- basicphp/app/views/home.php
+++ basicphp/app/views/home.php
@@ -1,10 +1,122 @@
 <!doctype html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<title>PHP Home</title>
+   <meta charset="UTF-8">
+   <title>Application</title>
+   <link rel="stylesheet" href="css/bootstrap.min.css">
+   <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-	<h1>Hello from Home</h1>
+   <div class="navbar navbar-default navbar-static-top">
+      <div class="container">
+         <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+               <span class="icon-bar"></span>
+               <span class="icon-bar"></span>
+               <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Tester</a>
+         </div>
+         <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+               <li class="active"><a href="#">String</a></li>
+               <li><a href="#about">About</a></li>
+               <li><a href="#contact">Contact</a></li>
+            </ul>
+         </div><!--/.nav-collapse -->
+      </div>
+   </div>
+   <div class="container">
+      <div class="row">
+         <div class="col-md-12">
+            <h1 class="text-center">String Manipulation</h1>
+         </div>
+         <div class="col-md-6">
+            <form role="form">
+               <input type="text" class="form-control input-lg" placeholder="Enter String To Test">
+               <div class="radio">
+                  <label>
+                     <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
+                     count number of characters
+                  </label>
+               </div>
+               <div class="radio">
+                  <label>
+                     <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2" data-toggle="modal" href="#myModal">
+                     find specific keyword in text
+                  </label>
+               </div>
+               <div class="radio">
+                  <label>
+                     <input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">
+                     determine no. of words
+                  </label>
+               </div>
+               <div class="radio">
+                  <label>
+                     <input type="radio" name="optionsRadios" id="optionsRadios4" value="option4">
+                     count specific character
+                  </label>
+               </div>
+               <div class="radio">
+                  <label>
+                     <input type="radio" name="optionsRadios" id="optionsRadios5" value="option5">
+                     split words
+                  </label>
+               </div>
+               <div class="radio">
+                  <label>
+                     <input type="radio" name="optionsRadios" id="optionsRadios6" value="option6">
+                     replace word
+                  </label>
+               </div>
+               <div class="form-group">
+                  <button type="submit" class="btn btn-primary btn-lg">Run</button>
+               </div>
+            </form>
+         </div>
+         <div class="col-md-6">
+            <form role="form">
+               <div class="form-group has-success">
+                  <input type="text" class="form-control input-lg" placeholder="Result..." disabled>
+               </div>
+               <div class="form-group">
+                  <h3>Code</h3>
+                  <pre>&lt;?php <br>$num = 12;<br>echo gettype($num);<br>?&gt;</pre>
+                  <h4>Description</h4>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, aspernatur, laboriosam, quae, fugiat recusandae qui aliquid reiciendis eveniet saepe voluptatum deleniti exercitationem sint quisquam consectetur repudiandae tempore ut mollitia assumenda?</p>
+               </div>
+            </form>
+         </div>
+            
+            
+
+         <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+               <div class="modal-content">
+                  <div class="modal-header">
+                     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                     <h4 class="modal-title">Modal title</h4>
+                  </div>
+                  <div class="modal-body">
+                     <form role="form">
+                        <div class="form-group">
+                           <input type="text" class="form-control input-lg" placeholder="Result...">
+                        </div>
+                        <div class="form-group">
+                           <input type="text" class="form-control input-lg" placeholder="Result...">
+                        </div>
+                     </form>
+                  </div>
+                  <div class="modal-footer">
+                     <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+                  </div>
+               </div><!-- /.modal-content -->
+            </div><!-- /.modal-dialog -->
+         </div><!-- /.modal -->
+      </div>
+   </div>
+   <script src="js/jquery-2.1.0.min.js"></script>
+   <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
--- basicphp/public/index.php
+++ basicphp/public/index.php
@@ -2,6 +2,7 @@
 
 define('DD', __DIR__ . '/../');
 
+ob_start(); // Output buffering start
 require DD . '/app/views/home.php';
-
+ob_end_flush(); // Output Buffering release
  ?>
